327b9c5480abec7982a4982047e81948b8988b25
bibeezi <44236242+bibeezi@users.noreply.github.com>
2020-04-07 13:47:54 +0100

    rain won't disappear

--- java/src/c18326906/Raindrop.java
+++ java/src/c18326906/Raindrop.java
@@ -1,28 +1,46 @@
 package c18326906;
 
-class Raindrop extends Yolk
+import processing.core.*;
+
+public class Raindrop
 {
+    Yolk egg;
+
     float x;
     float y1;
     float y2;
     int rainlength = 60;
 
-    Raindrop()
+    public Raindrop(Yolk egg)
     {
-        x = RandomNumber();
-        y1 = RandomNumber();
+        this.egg = egg; 
+
+        x = egg.RandomNumber();
+        y1 = egg.RandomNumber();
         y2 = y1 + rainlength;
     }
 
-    void update()
+    public void update()
     {
         y1++;
         y2++;
 
-        if(y2 > height)
+        if(y2 > egg.height)
         {
             y1 = 0;
             y2 = y1 + rainlength;
         }
+    }
+
+    public void display()
+    {
+        float colour = PApplet.map(egg.RandomNumber(), 0, 1000, 0, 255);
+
+        egg.stroke(colour, 255, 255);
+        egg.line(x, y1, x, y2);
+
+        egg.noStroke();
+        egg.fill(0, 0, 0);
+        egg.ellipse(egg.width / 2, egg.height, 300, 500);
     }
 }
--- java/src/c18326906/Yolk.java
+++ java/src/c18326906/Yolk.java
@@ -4,10 +4,10 @@ import ie.tudublin.Visual;
 import java.util.Random; 
 
 public class Yolk extends Visual
-{
-    int boundary = 1000;
-
-    Raindrop[] rain = new Raindrop[boundary];
+{   
+    int limit = 1000;
+    
+    Raindrop[] rain = new Raindrop[limit];
 
     public void settings()
     {
@@ -17,11 +17,11 @@ public class Yolk extends Visual
 
     public void keyPressed()
     {
-        if (key == ' ')
-        {
+        //if (key == ' ')
+        //{
             getAudioPlayer().cue(0);
             getAudioPlayer().play();
-        }
+        //}
     }
 
     public void setup()
@@ -32,46 +32,31 @@ public class Yolk extends Visual
         //loadAudio("BassnectarBassHead.mp3");
         loadAudio("KidCudiCreepers.mp3");
 
-        for(int i = 0; i < rain.length; i++)
-        {
-            rain[i] = new Raindrop();
-        }
-    }
-
-    public float RandomNumber()
-    {
-        Random rand = new Random();
-        return rand.nextInt(width);
-    }
-
-    public void display(float x, float y1, float y2)
-    {
+        background(0);
         colorMode(HSB);
-        float colour = map(RandomNumber(), 1, 1000, 0, 255);
-        stroke(colour, 255, 255);
-        line(x, y1, x, y2);
-        
+
         for(int i = 0; i < rain.length; i++)
         {
-            rain[i].update();
+            rain[i] = new Raindrop(this);
         }
-
-        //noStroke();
-        fill(0, 0, 0);
-        ellipse(width / 2, height, 300, 500);
     }
 
     public void draw()
     {
-        background(0);
-
         calculateAverageAmplitude();
 
-        int rainAmount = ceil(map(getSmoothedAmplitude() * 1000, 0, 250, 0, 250));
+        int rainAmount = ceil(map(getSmoothedAmplitude() * 100, 0, 30, 0, limit));
 
         for(int i = 0; i < rainAmount; i++)
         {
-            display(rain[i].x, rain[i].y1, rain[i].y2);
+            rain[i].display();
+            rain[i].update();
         }
+    }
+
+    public int RandomNumber()
+    {
+        Random rand = new Random();
+        return rand.nextInt(width);
     }
 }
